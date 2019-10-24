3a9269b5ce5e808ca69e6da21c9efdd0058e02ba
kkkumakk <10colorharu12262ezweb.ne.jp>
2019-10-24 09:35:45 +0900

    日付の変更

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+'use strict'
+
+// let dateToday = new Date();
+// let todayYear = dateToday.getFullYear();
+// let todayMonth = dateToday.getMonth() + 1;
+// let todayDate =  dateToday.getDate();
+
 const app = new Vue({
   el: '#app',
   data: {
@@ -8,20 +15,23 @@ const app = new Vue({
     pageSizes: [20,50,100],
     sortBy: '人気順',
     sortBys: ['新着順', '人気順'],
-    reqSortBy: 'popularity'
+    reqSortBy: 'popularity',
+    ok: false
   },
   watch: {
     keyword: function() {
+      this.ok = false
       this.message = "Waiting for you to stop typing ..."
       this.debouncedGetAnswer()
     },
     pageSize: function() {
+      this.ok = false
       this.debouncedGetAnswer()
     },
     sortBy: function() {
+      this.ok = false
       this.debouncedGetAnswer()
-    },
-
+    }
   },
   created: function() {
     this.message = 'キーワードを入力してください'
@@ -35,6 +45,7 @@ const app = new Vue({
         return
       }
 
+      this.ok = false
       this.message = 'Loading ...'
 
       if(this.sortBy === '人気順') {
@@ -43,18 +54,27 @@ const app = new Vue({
         this.reqSortBy = 'publishedAt'
       }
 
+      // let url = 'https://newsapi.org/v2/everything?' +
+      //     'q=' + this.keyword + '&' +
+      //     'from=' + todayYear + '-' + todayMonth + '-' + todayDate + '&' +
+      //     'sortBy=' + this.reqSortBy + '&' +
+      //     'pageSize=' + this.pageSize + '&' +
+      //     'apikey=' + '2dac0b01ed64490faa1e0d3c762ed5ae'
+
       let url = 'https://newsapi.org/v2/everything?' +
           'q=' + this.keyword + '&' +
-          'from=2019-09-18&' +
+          'from=2019-10-01' + '&' +
           'sortBy=' + this.reqSortBy + '&' +
           'pageSize=' + this.pageSize + '&' +
-          'apiKey=ea1eaa338f0048d2919407fb30fdc049'
+          'apikey=' + '2dac0b01ed64490faa1e0d3c762ed5ae'
 
       axios.get(url)
       .then((response) => {
         this.results = response.data.articles
+        console.log(url);
       })
       .finally((response) => {
+        this.ok = true
         if(this.results.length === 0) {
           this.message = '検索結果がありません'
         } else {
